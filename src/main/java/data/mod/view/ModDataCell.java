025fdfb4ef376afd546889238afa4ac44b3718d2
Erik Wessel <44678770+DunkleMango@users.noreply.github.com>
2019-12-27 17:10:31 +0100

    visual update of the mod table-cells

--- src/main/java/data/mod/view/ModDataCell.java
+++ src/main/java/data/mod/view/ModDataCell.java
@@ -16,21 +16,14 @@ public class ModDataCell extends ListCell<ModData> {
     private HBox hBox = new HBox();
     private Label label = new Label("(empty)");
     private ImageView imageView = new ImageView();
-    private Image imageNew = new Image(getClass().getResourceAsStream("/application/new.png"));
-    private ImageView imageView2 = new ImageView();
+    //private Image imageNew = new Image(getClass().getResourceAsStream("/application/new.png"));
     private ModData lastItem;
 
     public ModDataCell() {
         super();
         label.setWrapText(true);
-        label.setFont(new Font("Cambria", 16));
+        label.setFont(new Font(label.getFont().getFamily(), 16));
         label.setPadding(new Insets(5));
-        imageView.setPreserveRatio(true);
-        imageView.setFitHeight(100);
-        imageView.setFitWidth(100);
-        imageView2.setPreserveRatio(true);
-        imageView2.setFitHeight(32);
-        imageView2.setFitWidth(32);
         hBox.setSpacing(10);
         hBox.getChildren().addAll(label);
     }
@@ -45,7 +38,6 @@ public class ModDataCell extends ListCell<ModData> {
         } else {
             lastItem = item;
             label.setText(item.getTitle()!=null ? item.getTitle() : "<null>");
-            //imageView.setImage(item.getPreviewImage());
             //imageView2.setImage(imageNew);
             setGraphic(hBox);
         }
